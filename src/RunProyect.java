b43c5b14297502620458f658b11d31d80051f5c5
EmilioPiedra <espiedra1@utpl.edu.ec>
2022-05-13 16:57:50 -0500

    Prueba Parcial 2

--- src/RunProyect.java
+++ src/RunProyect.java
@@ -3,7 +3,8 @@ public class RunProyect {
     public static void main(String[] args) {
         Estudiante obj = new Estudiante("Emilio Piedra", "123@utpl.edu.ec");
         Asignatura objet = new Asignatura("Fisica", 2);
-        Actividad objeto ;
+        Actividad objeto  new Actividad("Ejercicios MRU", fisica.com);
+
 
 
     }
